802a59a8c4b1a2713e441ba57096fceced32b942
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-13 09:01:14 +0530

    refactored

--- src/com/bridgelabz/Gamblingsimulator.java
+++ src/com/bridgelabz/Gamblingsimulator.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class Welcome {
+public class Gamblingsimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator:");
     }
