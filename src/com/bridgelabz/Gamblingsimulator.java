41e3c22d9cc9026ddcfa39550eb6da4b69e3d366
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-13 09:03:43 +0530

    refactored & changed into one class first commit

--- src/com/bridgelabz/Gamblingsimulator.java
+++ src/com/bridgelabz/Gamblingsimulator.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class Gamblingsimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator:");
+
+        int STAKE_FOR_Day = 100;
+        int BET_FOR_GAME = 1;
     }
 }
