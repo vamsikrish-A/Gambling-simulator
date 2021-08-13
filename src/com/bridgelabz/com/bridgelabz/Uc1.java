28bf6cbfbabb546baabefa58a5051fd02f8d7d94
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-13 08:43:16 +0530

    first commit

---
+++ src/com/bridgelabz/com/bridgelabz/Uc1.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.com.bridgelabz;
+
+
+public class Uc1 {
+    public static void main(String[] args) {
+        int stakePerDay = 100;
+        int betForEverygame = 1;
+    }
+}
